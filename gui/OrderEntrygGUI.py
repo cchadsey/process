9b2891c07c2fc3f408e9efca3a1d7d9a9e4bb3e0
Camden <cbchadsey@gmail.com>
2024-05-13 13:36:40 -0500

    update fixed filedialog opening at run.

--- gui/OrderEntrygGUI.py
+++ gui/OrderEntrygGUI.py
@@ -1,22 +1,27 @@
 import tkinter as tk
-from tkinter.filedialog import askopenfile
-from tkinter.filedialog import askdirectory
+from tkinter import filedialog as fd
 
-warnmessage = 'Placeholder'
-donejob = 'Jobs Done!'
+#warnmessage = 'Placeholder'
+#donejob = 'Jobs Done!'
 
+def choose_file():
+    file_path = fd.askopenfilename(title='Select a File')
+    if file_path:
+        order.config(text=f"Selected File: {file_path}")
+
+def choose_folder():
+    folder_path = fd.askdirectory(title = 'Select an Output Folder')
+    if folder_path:
+        folder.config(text=f'Selected Folder: {folder_path}')
 
 
 m = tk.Tk()
+m.title('VIP Supplier Order Entry')
 
+folder = tk.Button(m, text =f'Select Folder', width= 25, command = choose_folder)
+order = tk.Button(m, text=f'Select File', width = 25, command = choose_file)
 
-
-title = tk.Label(m, text = 'VIP Supplier Order Entry')
-
-folder = tk.Button(m, text='Folder', width= 25, command= askdirectory())
-file = tk.Button(m, text='File', width = 25, command = askopenfile())
-
-tk.Label(m, text = 'Case count Column').grid(row = 3)
+colLabel = tk.Label(m, text = 'Case count Column')
 col = tk.Entry(m)
 
 
@@ -26,9 +31,9 @@ cancel = tk.Button(m, text='Cancel', width=10, command = m.destroy)
 
 
 #arrange main window in grid
-title.grid(row= 0 , column = 0, columnspan = 2)
 folder.grid(row = 1, column = 0)
-file.grid(row = 2, column = 0)
+order.grid(row = 2, column = 0)
+colLabel.grid(row=3, column = 0)
 col.grid(row=3, column = 0)
 ok.grid(pady = 50,row = 5, column = 0)
 cancel.grid(pady = 50, row = 5, column = 1, padx = 20)
